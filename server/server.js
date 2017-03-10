9ebd62fa6358b2170264f183b0dbe3496bc7ea2a
Kostya M <ozariss15@gmail.com>
2017-03-10 02:46:40 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
 	})
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find()
+		.then((todos) => {
+			res.send({todos});
+		}, (e) => {
+			res.statur(400).send(e);
+		})
+});
 
 
 app.listen(3000, () => {
