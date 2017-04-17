b98d17810bec5d73a184a1ab4b8980263b7cf996
Kostya M <ozariss15@gmail.com>
2017-04-18 00:07:10 +0300

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,10 +44,29 @@ app.get('/todos/:id', (req, res) => {
 	Todo.findById(id)
 		.then((todo) => {
 			if (todo) {
-				res.status(200).send({todo});
+				res.send({todo});
 			} else {
 				res.status(404).send();
 			}
+		}).catch((e) => {
+			res.status(400).send();
+		})
+});
+
+app.delete('/todos/:id', (req, res) => {
+	const {id} = req.params;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	};
+
+	Todo.findByIdAndRemove(id)
+		.then((todo) => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+
+			res.send({todo});
 		}).catch((e) => {
 			res.status(400).send();
 		})
